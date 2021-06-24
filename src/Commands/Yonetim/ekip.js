7c1fe332223885c44607edebc16c39d5fb675702
Brita <82638394+Britaaaaaxd@users.noreply.github.com>
2021-06-24 20:28:13 +0300

    Add files via upload

---
+++ src/Commands/Yonetim/ekip.js
@@ -0,0 +1,17 @@
+const conf = require("../../configs/config.json");
+const ayar = require("../../configs/settings.json")
+const emoji = require("../../configs/emoji.json")
+
+module.exports = {
+  conf: {
+    aliases: ["ekip","crew"],
+    name: "ekip",
+    help: "ekip <ekle / sil / liste>"
+  },
+
+  run: async (client, message, args, embed) => {
+/*
+Bir Kaç Hatası Vardı 
+Düzeltip Paylaşacağım
+*/
+}}
