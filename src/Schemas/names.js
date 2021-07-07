76c29f9369ac87db74e3f7a3e61b2dc61a4eb7b0
Brita <82638394+Britaaaaaxd@users.noreply.github.com>
2021-07-07 16:13:11 +0300

    Delete src directory

--- src/Schemas/names.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const schema = Schema({
-	guildID: { type: String, default: "" },
-	userID: { type: String, default: "" },
-	names: { type: Array, default: [] },
-});
-
-module.exports = model("britaNames", schema);
